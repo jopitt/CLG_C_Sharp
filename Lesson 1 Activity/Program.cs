97c8a67f3cd62d75c5d7da7e21a1220caa91f1e5
Jo Pittendreigh <91508510+jopitt@users.noreply.github.com>
2021-10-03 10:21:37 +1000

    Add files via upload
    Lesson 1 Activity

---
+++ Lesson 1 Activity/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson_1_Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("So now we’ve begun upon our road");
+            Console.WriteLine("Of learning C#, as we learn to code");
+      }
+    }
+}
