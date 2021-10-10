84584411fc3b0932bdbf8c015527135c86574a9d
Jo Pittendreigh <91508510+jopitt@users.noreply.github.com>
2021-10-10 10:20:53 +1000

    Split script into multiple lines

--- Lesson 2 Activity/Program.cs
+++ Lesson 2 Activity/Program.cs
@@ -23,7 +23,12 @@ namespace Lesson_2_Activity
 
 
 
-            Console.WriteLine($"{name.Trim()} went on a quest to find the last {mythicalCreature.Trim().ToLower()} of {destination.Trim()}. {name.Trim()} took the {transport.Trim().ToLower()} to {destination.Trim()} and was sitting {adverb.Trim().ToLower()} by themself, when suddenly hundreds of {noun.Trim().ToLower()} appeared and used their secret power of {superpower.Trim().ToLower()}. {name.Trim()} was so surprised that they forgot all about their quest, got the next {transport.Trim().ToLower()} home and lived happily ever after.");
+            Console.WriteLine($"{name.Trim()} went on a quest to find the last {mythicalCreature.Trim().ToLower()} of {destination.Trim()}.");
+            Console.WriteLine($"{name.Trim()} took the {transport.Trim().ToLower()} to {destination.Trim()} and was sitting {adverb.Trim().ToLower()}");
+            Console.WriteLine($"by themself, when suddenly hundreds of {noun.Trim().ToLower()} appeared");
+            Console.WriteLine($"and used their secret power of {superpower.Trim().ToLower()}. {name.Trim()} was so surprised");
+            Console.WriteLine($"that they forgot all about their quest, got the next {transport.Trim().ToLower()} home");
+            Console.WriteLine($"and lived happily ever after.");
         }
     }
 }
