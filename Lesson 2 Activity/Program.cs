305ac0212d8db6d4791d16fff2bad6c47179cbff
Jo Pittendreigh <91508510+jopitt@users.noreply.github.com>
2021-10-10 10:36:16 +1000

    Include method in ReadLine and clean up script.

--- Lesson 2 Activity/Program.cs
+++ Lesson 2 Activity/Program.cs
@@ -7,27 +7,27 @@ namespace Lesson_2_Activity
         static void Main(string[] args)
         {
             Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
+            var name = Console.ReadLine().Trim();
             Console.WriteLine("What is your favourite mode of transport?");
-            var transport = Console.ReadLine();
+            var transport = Console.ReadLine().Trim().ToLower();
             Console.WriteLine("Pick an out of the way travel destination");
-            var destination = Console.ReadLine();
+            var destination = Console.ReadLine().Trim();
             Console.WriteLine("Pick an adverb e.g. playfully, sadly");
-            var adverb = Console.ReadLine();
+            var adverb = Console.ReadLine().Trim().ToLower();
             Console.WriteLine("Pick a plural noun");
-            var noun = Console.ReadLine();
+            var noun = Console.ReadLine().Trim().ToLower();
             Console.WriteLine("What's your favourite mythical creature?");
-            var mythicalCreature = Console.ReadLine();
+            var mythicalCreature = Console.ReadLine().Trim().ToLower();
             Console.WriteLine("Pick a superpower");
-            var superpower = Console.ReadLine();
+            var superpower = Console.ReadLine().Trim().ToLower();
 
 
 
-            Console.WriteLine($"{name.Trim()} went on a quest to find the last {mythicalCreature.Trim().ToLower()} of {destination.Trim()}.");
-            Console.WriteLine($"{name.Trim()} took the {transport.Trim().ToLower()} to {destination.Trim()} and was sitting {adverb.Trim().ToLower()}");
-            Console.WriteLine($"by themself, when suddenly hundreds of {noun.Trim().ToLower()} appeared");
-            Console.WriteLine($"and used their secret power of {superpower.Trim().ToLower()}. {name.Trim()} was so surprised");
-            Console.WriteLine($"that they forgot all about their quest, got the next {transport.Trim().ToLower()} home");
+            Console.WriteLine($"{name} went on a quest to find the last {mythicalCreature} of {destination}.");
+            Console.WriteLine($"{name} took the {transport} to {destination} and was sitting {adverb}");
+            Console.WriteLine($"by themself, when suddenly hundreds of {noun} appeared");
+            Console.WriteLine($"and used their secret power of {superpower}. {name} was so surprised");
+            Console.WriteLine($"that they forgot all about their quest, got the next {transport} home");
             Console.WriteLine($"and lived happily ever after.");
         }
     }
