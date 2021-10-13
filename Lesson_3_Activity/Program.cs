867412b4fea74d5e7527df686a23ceb2d22dee1a
Jo Pittendreigh <91508510+jopitt@users.noreply.github.com>
2021-10-14 09:39:19 +1000

    Add files via upload
    1. calculate savings after the given number of months
    2. calculate savings compounded annually with term of investment in years
    3. calculate savings compounded monthly with term of investment in years, i.e. compounded 12 times in 1 year (with regular monthly deposits made at the end of each month)

---
+++ Lesson_3_Activity/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Lesson_3_Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //calculate savings after the given number of months:
+
+            Console.WriteLine("Initial deposit");
+            var deposit = double.Parse(Console.ReadLine());
+            Console.WriteLine("Monthly interest rate(%)");
+            var monthlyRateAsPercentage  = double.Parse(Console.ReadLine());
+            Console.WriteLine("Term of investment (months)");
+            var time = int.Parse(Console.ReadLine());
+
+            //
+            var savings = deposit * Math.Pow(1 + (monthlyRateAsPercentage/100),time);
+
+            Console.WriteLine(Math.Round(savings, 2));
+            */
+
+            /*
+            //calculate savings compounded annually with term of investment in years:
+            Console.WriteLine("Initial deposit");
+            var deposit = double.Parse(Console.ReadLine());
+            Console.WriteLine("Annual interest rate(%)");
+            var annualRateAsPercentage  = double.Parse(Console.ReadLine());
+            Console.WriteLine("Term of investment (years)");
+            var time = int.Parse(Console.ReadLine());
+
+            var savings = deposit * Math.Pow(1 + (annualRateAsPercentage/100),time);
+
+            Console.WriteLine(Math.Round(savings, 2));
+            */
+            
+            
+            //calculate savings compounded monthly with term of investment in years, i.e. compounded 12 times in 1 year (with regular monthly deposits made at the end of each month):
+
+            Console.WriteLine("Initial deposit");
+            var deposit = double.Parse(Console.ReadLine());
+            Console.WriteLine("Monthly payment");
+            var monthlyPayment = double.Parse(Console.ReadLine());
+            Console.WriteLine("Annual interest rate(%)");
+            var annualRateAsPercentage  = double.Parse(Console.ReadLine());
+            Console.WriteLine("Number of times that interest is compounded per year");
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Term of investment (years)");
+            var time = int.Parse(Console.ReadLine());
+
+            var numberTime = number * time;
+
+            var savings = (deposit * Math.Pow(1 + (annualRateAsPercentage/100/number),numberTime)) + (monthlyPayment * (Math.Pow(1 + (annualRateAsPercentage/100/number),numberTime) - 1)/(annualRateAsPercentage/100/number));
+
+            Console.WriteLine(Math.Round(savings, 2));
+            
+    
+        }
+    }
+}
