70d4233baed6075078a6b192f084c2a193350fd9
Jo Pittendreigh <91508510+jopitt@users.noreply.github.com>
2021-12-12 09:10:24 +1000

    Fixed formatting

--- Lesson_10_Activity/RideShare/Program.cs
+++ Lesson_10_Activity/RideShare/Program.cs
@@ -8,64 +8,63 @@ namespace RideShare.Models
     {
         static void Main(string[] args)
         {
-			var randomGenerator = new Random();
-			double randomDistance = randomGenerator.NextDouble();
-			randomDistance = Math.Round(randomDistance, 1);
-		
-			var car1 = new Car(4.5);
+		var randomGenerator = new Random();
+		double randomDistance = randomGenerator.NextDouble();
+		randomDistance = Math.Round(randomDistance, 1);
 
-			var car2 = new Car(1.2);
+		var car1 = new Car(4.5);
 
-			var car3 = new Car(randomDistance);
+		var car2 = new Car(1.2);
 
-			var luxuryCar1 = new LuxuryCar(0.6);
+		var car3 = new Car(randomDistance);
 
-			var luxuryCar2 = new LuxuryCar(0.2);
+		var luxuryCar1 = new LuxuryCar(0.6);
 
-			var luxuryCar3 = new LuxuryCar(randomDistance);
+		var luxuryCar2 = new LuxuryCar(0.2);
 
-			var extraLargeCar1 = new ExtraLargeCar(3.0);
+		var luxuryCar3 = new LuxuryCar(randomDistance);
 
-			var extraLargeCar2 = new ExtraLargeCar(randomDistance);
+		var extraLargeCar1 = new ExtraLargeCar(3.0);
 
-			var extraLargeCar3 = new ExtraLargeCar(randomDistance);
+		var extraLargeCar2 = new ExtraLargeCar(randomDistance);
+
+		var extraLargeCar3 = new ExtraLargeCar(randomDistance);
 
 
-			var vehicles = new List<Vehicle> { car1, car2, car3, luxuryCar1, luxuryCar2, luxuryCar3, extraLargeCar1, extraLargeCar2, extraLargeCar3 };
+		var vehicles = new List<Vehicle> { car1, car2, car3, luxuryCar1, luxuryCar2, luxuryCar3, extraLargeCar1, extraLargeCar2, extraLargeCar3 };
 
-			foreach (var vehicle in vehicles)
-			{
-				vehicle.ReportDistance();
-			}
+		foreach (var vehicle in vehicles)
+		{
+			vehicle.ReportDistance();
+		}
+		Console.WriteLine();
+
+		while(vehicles.Count > 0)
+		{
 			Console.WriteLine();
+			Console.WriteLine("Press any key to report distance of vehicle (to exit, press x):");
+			var input = Console.ReadLine().ToLower();
 
-			while(vehicles.Count > 0)
+			if(input == "x")
+			{
+				Console.WriteLine("You have exited the application.");
+				break;
+			}
+
+			var randomVehicle = randomGenerator.Next(0, vehicles.Count);
+
+			var selectedVehicle = vehicles[randomVehicle];
+			vehicles.RemoveAt(randomVehicle);
+
+			selectedVehicle.ReportDistance();
+
+			if(vehicles.Count == 0)
 			{
 				Console.WriteLine();
-				Console.WriteLine("Press any key to report distance of vehicle (to exit, press x):");
-				var input = Console.ReadLine().ToLower();
-
-				if(input == "x")
-				{
-					Console.WriteLine("You have exited the application.");
-					break;
-				}
-				
-				var randomVehicle = randomGenerator.Next(0, vehicles.Count);
-
-				var selectedVehicle = vehicles[randomVehicle];
-				vehicles.RemoveAt(randomVehicle);
-
-				selectedVehicle.ReportDistance();
-
-				if(vehicles.Count == 0)
-				{
-					Console.WriteLine();
-					Console.WriteLine("There are no more vehicles within your location.");
-				}
+				Console.WriteLine("There are no more vehicles within your location.");
 			}
+		}
         }
-	}
 }
 
 		
