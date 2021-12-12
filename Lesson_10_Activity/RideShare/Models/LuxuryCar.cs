000ed6b7d073bfc72deae1f8b9959a2526164ee9
Jo Pittendreigh <91508510+jopitt@users.noreply.github.com>
2021-12-12 10:54:31 +1000

    Updated random distance to within 5km

--- Lesson_10_Activity/RideShare/Models/LuxuryCar.cs
+++ Lesson_10_Activity/RideShare/Models/LuxuryCar.cs
@@ -8,6 +8,7 @@ namespace RideShare.Models
         {
             DistanceAwayInKm = distanceAwayInKm;
         }
+	
 		public override void ReportDistance()
 		{
 			Console.WriteLine($"You're in for a smooth ride! There is a luxury vehicle {DistanceAwayInKm}km away.");
